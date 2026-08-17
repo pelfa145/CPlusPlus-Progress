c3afcfe2da66639b44fca896db56c852153a1827
pelfa145 <cymig213@gmail.com>
2026-08-17 23:49:49 +0800

    bahala na muna ni

--- MiniProj/BankingProgram.cpp
+++ MiniProj/BankingProgram.cpp
@@ -1,39 +1,63 @@
 #include <iostream>
-#include <vector>
+#include <typeinfo>
 
-//pina oa mn ni oi, ayg palabi dong pwede rani himuag duha ka function tas i store ra ang variables sa main
-int main(){
+namespace bankAcc
+{
+    double balance = 0;
 
-    static double balance;
-    
-    
+    void deposit()
+    {
+        std::cout << "Input amount to deposit: ";
+        double amount;
+        std::cin >> amount;
+        balance += amount;
+    }
+    void withdraw()
+    {
+        std::cout << "Input amount to withdraw: ";
+        double amount;
+        std::cin >> amount;
+        balance -= amount;
+    }
+    void viewBalance()
+    {
+        std::cout << "Your current balance is: " << balance << "$" << std::endl;
+    }
+}
+
+void menu()
+{
+    bool quit = false;
+    while (!quit)
+    {
+        int choice;
+        std::cout << "Banking\n\n1.Deposit\n2.Withdraw\n3.View Balance\n4.Quit"<<std::endl;
+        std::cout << "Choose an option: "; std::cin >> choice;
+        if (typeid(choice) == typeid(int))
+        {   switch (choice)
+            {
+            case 1:
+                bankAcc::deposit();
+                break;
+            case 2:
+                bankAcc::withdraw();
+                break;
+            case 3:
+                bankAcc::viewBalance();
+                break;
+            case 4:
+                quit = true;
+                break;
+            default:
+                std::cout << "Choose an option between 1-4";
+            }
+        }
+        std::cout<<"Type an integer."<<std::endl; break;
+    }
+}
+
+int main()
+{
+    menu();
     return 0;
 }
-class Database{
-    private:
-        std::vector<BankAccount> accounts; 
-    
-    public:
-        void createAccount(const BankAccount& account){
-            accounts.push_back(account);
-
-        }
-    
-};
-
-class BankAccount{
-
-    private:
-        double balance = 0;
-        std::string name;
-    public:
-        BankAccount(std::string name)
-            : name(name), balance(balance){}
-        int getBalance() const{
-            return balance;
-        }
-        std::string getName() const{
-            return name;
-        }
-
-};
